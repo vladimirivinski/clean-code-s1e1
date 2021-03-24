e9cfe9aa6587485a62dc2383f2442657a3c422be
Vladimir <vladimir.ivinski89@gmail.com>
2021-03-24 20:34:55 +0300

    refactor: rename type to classes in html css and js files according to rule 3.4

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ var createNewTaskElement=function(taskString){
     checkBox.type='checkbox';
     editInput.type='text';
 
-    editInput.className='task';
-
+    editInput.className='task text';
+    
+    
 
     editButton.innerText='Edit'; //innerText encodes special characters, HTML does not.
     editButton.className='btn edit';
@@ -85,7 +86,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector('.text');
     var label=listItem.querySelector('label');
     var editBtn=listItem.querySelector('.edit');
     var containsClass=listItem.classList.contains('edit-mode');
