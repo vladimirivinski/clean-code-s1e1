b2730ababf7ff33ee860cd3ac88f4edefe910945
Vladimir <vladimir.ivinski89@gmail.com>
2021-03-25 10:24:58 +0300

    refactore: rename classes according to rule 3.4

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ var createNewTaskElement=function(taskString){
     checkBox.type='checkbox';
     editInput.type='text';
 
+    checkBox.className='checkbox';
+
     editInput.className='task text';
     
     
@@ -159,7 +161,7 @@ addButton.addEventListener('click',ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log('bind list item events');
 //select ListItems children
-    var checkBox=taskListItem.querySelector('input[type=checkbox]');
+    var checkBox=taskListItem.querySelector('.checkbox');
     var editButton=taskListItem.querySelector('button.edit');
     var deleteButton=taskListItem.querySelector('button.delete');
 
